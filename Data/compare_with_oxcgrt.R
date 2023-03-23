1d97a1bd62e8582891fb44ba6dcd75e909a270bd
sternenklar <76223786+sternenklar@users.noreply.github.com>
2023-03-23 01:02:55 +0100

    some corrections

--- Data/compare_with_oxcgrt.R
+++ Data/compare_with_oxcgrt.R
@@ -1,12 +1,15 @@
 rm(list=ls())
 library(data.table)
 library(curl)
-df_agr <- fread("https://github.com/sternenklar/A-Good-Reason/raw/main/complete%20data.csv")
-df_oxcgrt <- fread("https://github.com/OxCGRT/covid-policy-tracker/blob/master/data/OxCGRT_latest.csv?raw=true", select=c("Date", "CountryName", "RegionName", "C6_Stay at home requirements"))
-df_oxcgrt<-df_oxcgrt[(df_oxcgrt$RegionName==""),][,RegionName:=NULL] #necessary to delete all sub-national data to have distinct data points for each country and day
+library(readr)
+df_agr <- fread("https://raw.githubusercontent.com/sternenklar/A-Good-Reason/main/Data/complete%20data.csv")
+
+df_oxcgrt <- fread("https://github.com/OxCGRT/covid-policy-tracker/blob/master/data/OxCGRT_nat_latest.csv?raw=true", select=c("Date", "CountryName", "C6M_Stay at home requirements"))
+
 df_oxcgrt$Date=as.Date(as.character(df_oxcgrt$Date), format="%Y%m%d")
-setnames(df_oxcgrt, c("Date", "CountryName", "C6_Stay at home requirements"), c("date", "country", "C6_Stay_at_home_requirements"))
+setnames(df_oxcgrt, c("Date", "CountryName", "C6M_Stay at home requirements"), c("date", "country", "C6_Stay_at_home_requirements"))
 df_agr <- df_oxcgrt[df_agr, , on=c("country", "date")]
 df_agr <- df_agr[(C6_Stay_at_home_requirements %in% c(2,3) & stayhome_day=="no" & stayhome_night=="no") | (C6_Stay_at_home_requirements %in% c(0,1) & (stayhome_day!="no" | stayhome_night!="no")), difference_with_oxcgrt:="yes"][is.na(difference_with_oxcgrt), difference_with_oxcgrt:="no"]
 df_agr[,C6_Stay_at_home_requirements:=NULL]
-.
+
+write_csv(df_agr, "C:/Users/jvona/Documents/A Good Reason/data/complete data_compared.csv")
